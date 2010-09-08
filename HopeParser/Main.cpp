e5ac988003e0a48dc0e02706ac29cd3e243e16f2
vinc.demaio <vinc.demaio@ecc54513-6cf9-490c-0000-e2ee7fdbb3bd>
2010-09-08 20:44:37 +0000

    Issue minori risolte, i float ancora non vengono letti per bene

--- HopeParser/Main.cpp
+++ HopeParser/Main.cpp
@@ -2,8 +2,11 @@
 #include "EarthquakeParser.h"
 #include <iostream>
 #include <stdio.h>
+#include "normeAntisismiche-parse.h"
+#include "utilities.h"
 FILE *fin = fopen("fileIntermedio.bd", "r");
 int curr_lineno = 1;
+YYSTYPE normeAntisismiche_yylval;
 // Entry Point
 int main(int argc, char * argv[]) {
 
